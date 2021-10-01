f43e9528f566be42374792a1b4fe5e4a108c8310
Anastasia <Anastasia@10.0.0.29>
2021-09-30 22:07:02 -0400

    commit java is fun...

--- src/demo/GetDemo.java
+++ src/demo/GetDemo.java
@@ -2,9 +2,17 @@ package demo;
 
 public class GetDemo {
 	
+	
 	public static void main(String[] args) {
 		System.out.println("Git demo.....");
+		 	
+boolean isSunday = true;
 		
+		if(isSunday) {
+			System.out.println("abc");
+		}else {
+			System.out.println("Xyz");
+		}
 		
 		
 		
